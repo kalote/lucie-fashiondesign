12c34e9e60c147f2c02bc9ffec4e9a70b2b9f4f1
Johann Bich <johann.bich@C02TRPE8HV22.local>
2018-07-25 19:02:37 +0900

    style for iphone

--- src/components/Container.js
+++ src/components/Container.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 const Wrapper = styled.section`
   margin: 0 auto;
   max-width: 1200px;
-  padding: 6em 1.5em 2em;
+  padding: 3em 1.5em 2em;
 `
 
 const Container = props => {
